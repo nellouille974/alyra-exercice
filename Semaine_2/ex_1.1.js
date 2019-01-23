360d195d7cb1a6186bc889b5c9d0868d9ba081eb
nellouille974 <lariviere.nelly@yahoo.fr>
2019-01-23 17:49:33 +0100

    Exercice jour 1 semaine 2

---
+++ Semaine_2/ex_1.1.js
@@ -0,0 +1,5 @@
+function calculerDifficulte(cibleActuel) {
+	var cibleMax = 2.7 * Math.pow(10,67);
+	return cibleMax / cibleActuel;
+}
+calculerDifficulte(1147152896345386682952518188670047452875537662186691235300769792000);
---
+++ Semaine_2/ex_1.2.js
@@ -0,0 +1,15 @@
+// function decrypt(bits) {
+// 	bits = bits.split("",2);
+// 	var bytes = parseInt(bits.toString().split("",2),16);
+// 	result = bits.toString().split(2,) + ("00" * (bytes-3));
+// 	return result;
+// }
+
+// function calculerDifficulte(cibleActuel) {
+// 	var cibleMax = 2.7 * Math.pow(10,67);
+// 	cibleActuel = decrypt(cibleActuel);
+// 	//console.log(cibleMax, cibleActuel);
+// 	return cibleMax / cibleActuel;
+// }
+
+// calculerDifficulte("0x1c0ae493");
