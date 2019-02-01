1a1acc11d5a5ccba977ecd70064f332c685d800b
nellouille974 <lariviere.nelly@yahoo.fr>
2019-02-01 15:27:58 +0100

    Ajouter des sponsors à une liste

---
+++ Semaine_3/Jour_2/ex_Exercice 3.2.2.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.25;
+contract Cogere {
+
+  mapping (address => uint) organisateurs;
+
+  constructor() internal {
+    organisateurs[msg.sender] = 100;
+  }
+
+  function transfererOrga(address orga, uint parts) public {
+    organisateurs[orga] += parts;
+    organisateurs[msg.sender] = sub(organisateurs[msg.sender],parts);
+  }
+
+  function estOrga(address orga) public view returns (bool)  {
+    if(organisateurs[orga] > 0) {
+      return true;
+    } else return false;
+  }
+
+    /**
+  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+}
+contract CagnotteFestival is Cogere {
+
+
+  mapping (address => uint) festivaliers;
+  uint private depensesTotales;
+  uint public montantMinSponsor;
+  address[] sponsors;
+
+  function acheterTicket() public payable {
+    require(msg.value > 500 finney, "Place à 0.5 Ether");
+    festivaliers[msg.sender];
+  }
+
+  function payer(address  destinataire, uint montant) public payable {
+    require(estOrga(msg.sender));
+    require(destinataire != address(0));
+    require(montant > 0);
+
+    destinataire.transfer(montant);
+  }
+
+  function sponsoriser() public payable {
+    require(msg.value >= montantMinSponsor);
+    sponsors[sponsors.length]=msg.sender;
+  }
+
+  
+  function comptabiliserDepenses(uint montant) private {
+    depensesTotales += montant;
+  }
+
+}
