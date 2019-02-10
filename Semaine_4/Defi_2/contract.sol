72579f08d6bd265f169f87e26b9db83161c5f01d
nellouille974 <lariviere.nelly@yahoo.fr>
2019-02-10 17:57:48 +0100

    Final version

--- Semaine_4/Defi_2/contract.sol
+++ Semaine_4/Defi_2/contract.sol
@@ -1,51 +1,135 @@
 pragma solidity ^0.5.3;
 pragma experimental ABIEncoderV2;
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract Illustrator {
+contract marketPlaceIllustrator {
     
-   using SafeMath for uint256;
-   //event NewOffer(uint256 id,uint256 enumeration, uint256 timeLimit,string state, string description, uint256 minReputation);
-   
-   struct Offer {
-       address user;
-       string nameOfUser;
-       uint256 enumeration;
-       uint256 timeLimit;
-       string state;
-       string description;
-       uint256 minReputation;
-       address[] listOfCandidates;
+    // Variable
+    enum stateOffer { OPEN , IN_PROGRESS , CLOSE }
+    
+    // Customer
+    struct Customer {
+        string name;
+        uint256 reputation;
+    }
+    
+    // Illustrator
+    struct Illustrator {
+        string name;
+        uint256 reputation;
+    }
+    
+    // Offer
+    struct Offer {
+        address customer;
+        address illustrator;
+        string title;
+        string description;
+        uint256 minReputation;
+        stateOffer state;
+        address[] listOfCandidates;
+        uint256 enumeration;
+        uint256 timeLimit;
+        bytes32 hashUrl;
+        bytes32 hashOffer;
+    }
+    
+    // Array
+    bytes32[] public offersHashDictionnary ; // liste de tous les hashs
+    
+    // Mapping
+    mapping(address => Customer) public customers;
+    mapping(address => Illustrator) public illustrators;
+    mapping(bytes32 => Offer) public offersMapping;
+    
+    // Event
+    event appliedToOffer(address[] offer);
+    
+    // Registration
+    function customerRegistration(string memory name) public {
+        Customer storage customer = customers[msg.sender];
+        customer.name = name;
+        customer.reputation = 1;
+    }
+    function illustratorRegistration(string memory name) public {
+        Illustrator storage illustrator = illustrators[msg.sender];
+        illustrator.name = name;
+        illustrator.reputation = 1;
+    }
+    
+    // Add offer
+    function addOffer(string memory title, string memory description,uint256 minReputation, uint256 enumeration, uint256 timeLimit) public payable returns (Offer memory) {
+        require(customers[msg.sender].reputation == 1);
+        Offer memory offer;
+        uint commision = enumeration + (enumeration * 2 / 100);
+        offer.customer = msg.sender;
+        offer.illustrator = address(0);
+        offer.title = title;
+        offer.description = description;
+        offer.minReputation = minReputation;
+        offer.state = stateOffer.OPEN;
+        offer.enumeration = msg.value + commision;
+        offer.timeLimit = timeLimit;
+        offer.hashUrl = 0;
+        bytes32  offerHash = generateOfferHash(offer);
+        offer.hashOffer = offerHash;
+        offersMapping[offerHash] = offer;
+        offersHashDictionnary.push(offerHash); 
+
+        return offer;
+    }
+    
+    // Return all offers
+    function offers() public view returns (Offer[] memory) {
+        Offer[] memory list = new Offer[](offersHashDictionnary.length);
+    for (uint i = 0; i < offersHashDictionnary.length; ++i) {
+        list[i] = offersMapping[offersHashDictionnary[i]];
+    }
+        return list;
+    }
+    
+    // Apply offer
+    function applyToOffer(bytes32  hashOffer) public {
+        Offer storage offer = offersMapping[hashOffer];
+        require(offer.state == stateOffer.OPEN );
+        require(illustrators[msg.sender].reputation >= offer.minReputation );
+        offer.listOfCandidates.push(msg.sender);
+        emit appliedToOffer(offer.listOfCandidates);
+
+    }
+    
+    // Hash offer address title and description 
+    function generateOfferHash(Offer memory offer) private pure returns (bytes32)  {
+        
+        return keccak256(abi.encodePacked(offer.customer, offer.title, offer.description));
+        
+    }
+    
+    // Accept offer
+    function acceptOffer(address illustrator , bytes32 hashOffer) public returns (Offer memory) {
+        Offer storage offer = offersMapping[hashOffer];
+        require(offer.customer == msg.sender);
+        require(offer.state == stateOffer.OPEN );
+        require(illustrators[illustrator].reputation >= offer.minReputation);
+        offer.illustrator = illustrator;
+        offer.state = stateOffer.IN_PROGRESS;
+        return offer;
+    }
+    
+    // Hash url of work
+   function hashUrlWork(string memory url) pure public returns (bytes32) {
+       return  keccak256(bytes(url));
    }
    
-   //array of request
-   Offer[] public offers;
-   //associate each user with a value for reputation
-   mapping (address => uint256) public reputation;
-   //associate name with address
-   mapping (address => string) public nameOfUser;
-   
-   function registration(address user, string memory name) public {
-        nameOfUser[user] = name;
-        reputation[user] += 1;
-   }
-   
-   function addOffer(address user, string memory nameOfUser, uint256 enumeration, uint256 timeLimit,string memory state, string memory description, uint256 minReputation) public {
-       require(reputation[user] >= 1);
-       uint commision = enumeration * 2 / 100;
-       uint montant = enumeration + commision;
-       address[] memory addresses;
-       offers.push(Offer(user, nameOfUser, montant, timeLimit, state = "OPEN" ,description, minReputation, addresses));
-       //emit NewOffer(id,enumeration, timeLimit, state, description, minReputation);
-   }
-   
-   function getOffers() view public returns  (Offer[] memory) {
-       return offers;
-   }
-   
-   function applyOffer(address independant) public {
+   // Delivery
+   function deliveryHashWork(bytes32 hashUrl, bytes32 hashOffer) public {
+        Offer storage offer = offersMapping[hashOffer];
+        require(offer.illustrator == msg.sender);
+        require(offer.state == stateOffer.IN_PROGRESS);
+        offer.hashUrl = hashUrl;
+        offer.state = stateOffer.CLOSE;
+        uint commision = offer.enumeration * 2 / 100;
+        msg.sender.transfer(offer.enumeration - commision);
        
    }
-   
-
+    
 }
--- Semaine_4/Defi_2/script.js
+++ Semaine_4/Defi_2/script.js
@@ -1,28 +1,48 @@
-async function createMetaMaskDapp() {
- try {
-   // Demande à MetaMask l'autorisation de se connecter
-   const addresses = await ethereum.enable();
-   const address = addresses[0]
-   // Connection au noeud fourni par l'objet web3
-   const provider = new ethers.providers.Web3Provider(ethereum);
-   const dapp = { address, provider };
-   console.log(dapp);
-   //contract
-   let abi = [
+// ABI contract
+const abi = [
 	{
-		"constant": true,
-		"inputs": [],
-		"name": "getOffers",
+		"constant": false,
+		"inputs": [
+			{
+				"name": "illustrator",
+				"type": "address"
+			},
+			{
+				"name": "hashOffer",
+				"type": "bytes32"
+			}
+		],
+		"name": "acceptOffer",
 		"outputs": [
 			{
 				"components": [
 					{
-						"name": "user",
+						"name": "customer",
 						"type": "address"
 					},
 					{
-						"name": "nameOfUser",
+						"name": "illustrator",
+						"type": "address"
+					},
+					{
+						"name": "title",
 						"type": "string"
+					},
+					{
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"name": "minReputation",
+						"type": "uint256"
+					},
+					{
+						"name": "state",
+						"type": "uint8"
+					},
+					{
+						"name": "listOfCandidates",
+						"type": "address[]"
 					},
 					{
 						"name": "enumeration",
@@ -33,7 +53,60 @@ async function createMetaMaskDapp() {
 						"type": "uint256"
 					},
 					{
-						"name": "state",
+						"name": "hashUrl",
+						"type": "bytes32"
+					},
+					{
+						"name": "hashOffer",
+						"type": "bytes32"
+					}
+				],
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "title",
+				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "minReputation",
+				"type": "uint256"
+			},
+			{
+				"name": "enumeration",
+				"type": "uint256"
+			},
+			{
+				"name": "timeLimit",
+				"type": "uint256"
+			}
+		],
+		"name": "addOffer",
+		"outputs": [
+			{
+				"components": [
+					{
+						"name": "customer",
+						"type": "address"
+					},
+					{
+						"name": "illustrator",
+						"type": "address"
+					},
+					{
+						"name": "title",
 						"type": "string"
 					},
 					{
@@ -43,6 +116,227 @@ async function createMetaMaskDapp() {
 					{
 						"name": "minReputation",
 						"type": "uint256"
+					},
+					{
+						"name": "state",
+						"type": "uint8"
+					},
+					{
+						"name": "listOfCandidates",
+						"type": "address[]"
+					},
+					{
+						"name": "enumeration",
+						"type": "uint256"
+					},
+					{
+						"name": "timeLimit",
+						"type": "uint256"
+					},
+					{
+						"name": "hashUrl",
+						"type": "bytes32"
+					},
+					{
+						"name": "hashOffer",
+						"type": "bytes32"
+					}
+				],
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "hashOffer",
+				"type": "bytes32"
+			}
+		],
+		"name": "applyToOffer",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "customerRegistration",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "hashUrl",
+				"type": "bytes32"
+			},
+			{
+				"name": "hashOffer",
+				"type": "bytes32"
+			}
+		],
+		"name": "deliveryHashWork",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "illustratorRegistration",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "offer",
+				"type": "address[]"
+			}
+		],
+		"name": "appliedToOffer",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "customers",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "reputation",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "url",
+				"type": "string"
+			}
+		],
+		"name": "hashUrlWork",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "illustrators",
+		"outputs": [
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "reputation",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "offers",
+		"outputs": [
+			{
+				"components": [
+					{
+						"name": "customer",
+						"type": "address"
+					},
+					{
+						"name": "illustrator",
+						"type": "address"
+					},
+					{
+						"name": "title",
+						"type": "string"
+					},
+					{
+						"name": "description",
+						"type": "string"
+					},
+					{
+						"name": "minReputation",
+						"type": "uint256"
+					},
+					{
+						"name": "state",
+						"type": "uint8"
+					},
+					{
+						"name": "listOfCandidates",
+						"type": "address[]"
+					},
+					{
+						"name": "enumeration",
+						"type": "uint256"
+					},
+					{
+						"name": "timeLimit",
+						"type": "uint256"
+					},
+					{
+						"name": "hashUrl",
+						"type": "bytes32"
+					},
+					{
+						"name": "hashOffer",
+						"type": "bytes32"
 					}
 				],
 				"name": "",
@@ -54,15 +348,57 @@ async function createMetaMaskDapp() {
 		"type": "function"
 	},
 	{
-		"constant": false,
+		"constant": true,
 		"inputs": [
 			{
-				"name": "user",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "offersHashDictionnary",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"name": "offersMapping",
+		"outputs": [
+			{
+				"name": "customer",
 				"type": "address"
 			},
 			{
-				"name": "nameOfUser",
+				"name": "illustrator",
+				"type": "address"
+			},
+			{
+				"name": "title",
 				"type": "string"
+			},
+			{
+				"name": "description",
+				"type": "string"
+			},
+			{
+				"name": "minReputation",
+				"type": "uint256"
+			},
+			{
+				"name": "state",
+				"type": "uint8"
 			},
 			{
 				"name": "enumeration",
@@ -73,135 +409,51 @@ async function createMetaMaskDapp() {
 				"type": "uint256"
 			},
 			{
-				"name": "state",
-				"type": "string"
+				"name": "hashUrl",
+				"type": "bytes32"
 			},
 			{
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"name": "minReputation",
-				"type": "uint256"
-			}
-		],
-		"name": "addOffer",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "offers",
-		"outputs": [
-			{
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"name": "nameOfUser",
-				"type": "string"
-			},
-			{
-				"name": "enumeration",
-				"type": "uint256"
-			},
-			{
-				"name": "timeLimit",
-				"type": "uint256"
-			},
-			{
-				"name": "state",
-				"type": "string"
-			},
-			{
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"name": "minReputation",
-				"type": "uint256"
+				"name": "hashOffer",
+				"type": "bytes32"
 			}
 		],
 		"payable": false,
 		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "reputation",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "nameOfUser",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"name": "name",
-				"type": "string"
-			}
-		],
-		"name": "registration",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
 		"type": "function"
 	}
 ];
-
-	let contratIllustrator = new ethers.Contract("0xe89141073d94cc332d2ce34f9cfaccb42cf29234", abi, dapp.provider);
-	let off = await contratIllustrator.getOffers();
-	console.log(off);
-	f = document.createDocumentFragment();
-	for(t of off){
-		d = document.createElement('div');
-		d.innerHTML = t.nameOfUser +" "+ t.enumeration.toString() +" "+ t.state +" "+ t.description +" "+ t.minReputation.toString();
-		f.appendChild(d);
-	}
-	document.body.appendChild(f);
+// Connection metaMask
+async function createMetaMaskDapp() {
+ try {
+   // Demande à MetaMask l'autorisation de se connecter
+   const addresses = await ethereum.enable();
+   const address = addresses[0]
+   // Connection au noeud fourni par l'objet web3
+   const provider = new ethers.providers.Web3Provider(ethereum);
+   const dapp = { address, provider };
+   const marketPlaceIllustrator = new ethers.Contract("0x57cd457cb96824a840b0fb0415ab4e90bed8679b", abi, dapp.provider.getSigner());
+   console.log(dapp);
+   // List Offers
+   let offers =  await marketPlaceIllustrator.offers();
+   // create fragment
+   const f = document.createDocumentFragment();
+   // loop offers
+   for(offer of offers) {
+	   	doc = document.createElement("div");
+	   	if(offer.state <= 1) {
+	   	doc.innerHTML = `<p>Titre :  ${offer.title} </p>
+	   					<p>Description : ${offer.description} </p>
+	   					<p>Réputation minimum requise : ${offer.minReputation} </p>
+	   					<p>Rénumération : ${offer.enumeration} </p>
+	   					<p>Délai : ${offer.timeLimit} </p>`;
+	   	f.appendChild(doc);
+	   } else {
+	   	doc.innerHTML = "Désolée, il n'y a pas d'offres pour le moment.";
+	   	f.appendChild(doc);
+	   }
+   }
+   document.body.appendChild(f);
+   console.log(offers);
  } catch(err) {
    // Gestion des erreurs
    console.error(err);
