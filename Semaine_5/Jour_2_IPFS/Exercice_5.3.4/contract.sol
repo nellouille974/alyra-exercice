bb6bd02b1cc015d117bb77ec755dcead018be923
nellouille974 <lariviere.nelly@yahoo.fr>
2019-02-14 17:21:02 +0100

    Exercice 5.3.4 - Créer un jeu de cartes à collectionner

---
+++ Semaine_5/Jour_2_IPFS/Exercice_5.3.4/contract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.3;
+pragma experimental ABIEncoderV2;
+contract Cartes {
+   string[] cartes;
+
+   function ajouterCarte(string memory s) public {
+       cartes.push(s);
+   }
+
+   function recuperer(uint ind) view public returns (string memory) {
+       return cartes[ind];
+   }
+   
+   function toutRecuperer() public view returns (string[] memory) {
+       return cartes;
+   }
+}
---
+++ Semaine_5/Jour_2_IPFS/Exercice_5.3.4/script.js
@@ -0,0 +1,107 @@
+// ABI contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "s",
+				"type": "string"
+			}
+		],
+		"name": "ajouterCarte",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "ind",
+				"type": "uint256"
+			}
+		],
+		"name": "recuperer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "toutRecuperer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const provider = new ethers.providers.Web3Provider(ethereum);
+const addresses =  ethereum.enable();
+const address = addresses[0]
+const dapp = { address, provider };
+const collectionCard = new ethers.Contract("0x1b6e533f7f127526b119a38b8640d7d5a3946f44", abi, dapp.provider.getSigner());
+console.log(dapp);
+
+const node = new Ipfs({ repo: 'ipfs-' + Math.random() });
+node.once('ready', () => {
+	node.id(function (err, id) {
+		if (err) {
+			throw err;
+		}
+		console.log("connecté, id : ", id);
+	});
+})
+
+// Ajouter une image
+function addImg() {
+	let image = document.getElementById("fichierImage").files[0];
+	const reader = new FileReader();
+	reader.readAsArrayBuffer(image);
+	reader.onloadend = function() {
+		node.add(new node.types.Buffer.from(reader.result)).then(r =>{
+			let rs = collectionCard.ajouterCarte(r[0].hash);
+			console.log(rs);
+		})  
+	}
+}
+
+// Afficher une image grace à son index
+async function recupImg(index) {
+	index = document.getElementById("index").value;
+	console.log(index);
+	let resultat = await collectionCard.recuperer(index);
+	console.log(resultat,"resultat");
+	node.cat(resultat, function (err,file) {
+  		if (err) {
+    		throw err;
+  		}
+  		console.log(file.toString('utf8'));
+  		document.getElementById("imageRecup").src = "data:image/jpg;base64," + file.toString('base64');
+	});
+}
+
+// Afficher les dix dernieres images ( provoque une erreur )
+
+// async function recupAllImg() {
+// 	let all = await collectionCard.toutRecuperer();
+// 	node.cat(all, function (err,file) {
+// 		let list = document.getElementById("list");
+// 		for(let i = 0;i < all.length && i < 10; i++){
+// 			list = '<img src="data:image/jpg;base64,"' + file.toString('base64') + '"/>';
+// 		}
+// 	})
+// }
